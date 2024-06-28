0e4f2b64b99178cc76c94f8cce3a7425319d9b1a
Tanzida Nowshin <nowshinsara555@gmail.com>
2024-06-28 09:24:21 +0600

    Our Recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header'
 import './App.css'
 import Banner from './components/Banner/Banner'
+import OurRecipes from './components/ourRecipes/OurRecipes'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header></Header>
       <Banner></Banner>
+      <OurRecipes></OurRecipes>
     </>
   )
 }
---
+++ src/components/ourRecipes/OurRecipes.jsx
@@ -0,0 +1,12 @@
+const OurRecipes = () => {
+    return (
+        <div className=" p-4 mx-auto">
+            <h1 className="text-3xl font-bold mx-auto">Our Recipes</h1>
+            <p>Our recipes are based on the secret and best recipes of my mother, who is a dedicated homeopathy doctor and a skincare enthusiast. She has a profound dedication to cooking and gardening, and I am excited to share all her special recipes with you here. I hope you will love exploring these dishes!
+</p>
+            
+        </div>
+    );
+};
+
+export default OurRecipes;
